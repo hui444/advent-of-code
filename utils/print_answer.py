1ac1edc874a78c80253f59038f211429f65648b4
hui <hui@huis-iMac.local>
2023-12-14 02:06:27 +0800

    feat: add util functions

---
+++ utils/print_answer.py
@@ -0,0 +1,4 @@
+# export PYTHONPATH=/Users/hui/Desktop/advent-of-code
+
+def print_answer(part_number, answer):
+    print(f"Part {part_number}: {answer}")
---
+++ utils/read_input.py
@@ -0,0 +1,3 @@
+def read_input(file_path):
+    f = open(f".{file_path}", "r")
+    return f.read()
